8190333c28cdbbfa6aeb1db4d486374a44ae18a1
Evgeniy <zelevzel@ya.ru>
2020-10-29 22:25:12 +0300

    feat: complete task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let nameArr = [];
+
+  if ((typeof (members) !== 'object') || (members === null) || (members.length === undefined)) {
+    return false;
+  }
+
+  members.forEach(item => {
+    if (typeof (item) !== 'string') {
+      return false;
+    }
+    for (let i = 0; i < item.length; i++) {
+      if (item[i] !== ' ') {
+        nameArr.push(item[i].toUpperCase());
+        break;
+      }
+    }
+  })
+
+  return nameArr.sort().join('');
+
+}
